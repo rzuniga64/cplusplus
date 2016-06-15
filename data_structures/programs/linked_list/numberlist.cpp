944c471fa5e5f25acd0186d87e0c2263b210f6ef
Raul Zuniga <rzuniga64@gmail.com>
2016-06-15 01:42:20 -0500

    Add data structures directory

--- data_structures/programs/linked_list/numberlist.cpp
+++ data_structures/programs/linked_list/numberlist.cpp
@@ -4,10 +4,18 @@
 
 using namespace std;
 
+// constructor sets up empty list
 NumberList::NumberList() {
     head = NULL;
 }
 
+/*
+ * Pointers + dynamic allocation => deep copy
+ * Don't copy any pointers (allocate new memory)
+ * Iterate over src list, append nodes to the new list
+ * Initialize head to NULL
+ * For each item in the src list (in order) append value to the list
+ */
 NumberList::NumberList(const NumberList &src) {
     head = NULL;
     ListNode *p = src.head;
@@ -17,6 +25,12 @@ NumberList::NumberList(const NumberList &src) {
     }
 }
 
+/*
+ *  Destructor: must deallocate all mondes used in the list
+ *  To do this, use list traversal to visit each node.
+ *  For each node save the address of the next node in a pointer.
+ *  Delete the node and advance the pointer.
+ */
 NumberList::~NumberList() {
     ListNode *p = head;
     ListNode *n;
@@ -27,6 +41,16 @@ NumberList::~NumberList() {
     }
 }
 
+/*
+ *  Append add new node to end of list
+ *  Algorithm:
+ *  1. Create a new node and store the data in it
+ *  2. If the list has no nodes
+ *          Make head point to the new node
+ *     Else
+ *          Find teh last node in the list
+ *          Make the lst node point to the new node
+ */
 void NumberList::appendNode(double num) {
     
     ListNode *newNode = new ListNode;
@@ -42,6 +66,26 @@ void NumberList::appendNode(double num) {
         p->next = newNode;
     }
 }
+
+/*
+ * 	insert:  Inserts a new node into the middle of a list.
+ * 	Uses two extra pointers:
+ * 	one to point to node before the insertion point
+ * 	one to point to the node after the insertion point [this one is optional]
+ *
+ * 	Insert node in a certain position
+ *
+ * 	Create the new node, store the data in it
+ * 	Use pointer p to traverse the list,
+ * 	    until it points to: node after insertion point or  NULL
+ * 	    as p is advancing, make n point to the node  before
+ *  if p points to first node (p is head, n was not set)
+ *      make head point to new node
+ *      make new node point to p’s node
+ *  else
+ *      make n’s node point to new node
+ *      make new node point to p’s node
+ */
 
 void NumberList::insertNode(double num) {
 
@@ -63,6 +107,26 @@ void NumberList::insertNode(double num) {
         newNode->next = p;
     }
 }
+
+/*
+ *  deleteNode: removes node from list, and deletes (Deallocates) the removed node.
+ *  Requires two pointers:
+ *      one to point to the node to be deleted
+ *      one to point to the node before the node to be deleted.
+ *      Change the pointer of the previous node to point to the node after the one to be deleted.
+ *      Now just “delete” the p node
+ *      use p to traverse the list, until it points to num or NULL
+ *      as p is advancing, make n point to the node before it
+ *
+ *      if (p is not NULL) //found!
+ *      if (p==head) //it’s the first node, and n is garbage
+ *          make head point to the second element
+ *          delete p’s node (the first node)
+ *      else
+ *          make n’s node point to what p’s node points to
+ *          delete p’s node
+ *      else: . . . p is NULL, not found do nothing
+ */
 
 void NumberList::deleteNode(double num) {
     ListNode *p = head;
@@ -92,5 +156,18 @@ void NumberList::displayList() {
     cout << endl;
 }
 
-void NumberList::operator= (const NumberList &rhs) { }
+/*
+ *  display
+ *  Visit each node in a linked list, to display contents, sum data, test data, etc.
+ *  Basic process:
+ *      set a pointer to point to what head points to
+ *      while pointer is not NULL
+ *      process data of current node
+ *      go to the next node by setting the pointer to the pointer field of the current node
+ *      end while
+ */
+
+void NumberList::operator= (const NumberList &rhs) {
+
+}
 
--- data_structures/programs/linked_list/numberlist.h
+++ data_structures/programs/linked_list/numberlist.h
@@ -1,6 +1,29 @@
 #ifndef NUMBERLIST_H
 #define NUMBERLIST_H
 
+/**
+ * A data structure representing a series of dynamically allocated nodes chained together in a sequence.
+ * Each node points to another node.
+ * Each nodes contains a data field (structure, object, etc) and a pointer that can point to another node.
+ * A separate pointer (the head) points to the first item in the list.
+ * The last element (the tail) points to nothing (NULL).
+ * An empty list contains no nodes. The list head points to NULL
+ *
+ *  Operations:
+ *  constructor:        creates an empty linked list
+ *  append:             add a link to the end of the list
+ *  insert:             add a node within the list
+ *  delete:             delete a link
+ *  display():          display the linked list
+ *  copy constructor (and operator=)
+ *
+ * Advantages of linked lists over arrays
+ * 1. A linked list can grow or shrink in size.
+ * 2. The programmer doesn't need to predict how many values could be in teh list.
+ * 3. The programmer doesn't need to resize and copy the list when it reaches a certain capacity.
+ * 4. When a value is inserted into or deleted from a linked list, none of the other nodes have to be moved.
+ */
+
 #include <cstddef>   // for NULL
 
 class   NumberList
